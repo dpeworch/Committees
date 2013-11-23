3f2ac182b13ef511dacb306a979210c72ef21fe1
dpeworch <dpeworch@mail.umw.edu>
2013-11-23 17:08:50 -0500

    search function plus a few small things

---
+++ committees/src/my/committee/Database.java
@@ -0,0 +1,93 @@
+
+package my.committee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Database {
+
+    private Connection conn;
+
+    public Database() throws InstantiationException, SQLException, IllegalAccessException {
+        conn = null;
+        init();
+    }
+
+    public void close() throws InstantiationException, SQLException, IllegalAccessException {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void init() throws InstantiationException, SQLException, IllegalAccessException {
+        try {
+            System.out.println("MySQL Connect Example.");
+            String url = "jdbc:derby://localhost:1527/";
+            String dbName = "db";
+            String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+            String userName = "admin";
+            String password = "password";
+
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            System.out.println("Connected to the database");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void insertComm(String input) {
+            /*query = "INSERT INTO APP.PROFS (FIRST_NAME, LAST_NAME, DEPARTMENT, SCHOOL)"
+                    + " VALUES (?, ?, ?, ?)";
+            PreparedStatement psInsert = conn.prepareStatement(query);
+            //psInsert.setInt(1, 1956);
+            psInsert.setString(1, "Karen");
+            psInsert.setString(2, "Anewalt");
+            psInsert.setString(3, "CPSC");
+            psInsert.setString(4, "UMW");
+            psInsert.executeUpdate();*/
+    }
+
+     public String search(String input) {
+         if (input.equals("")) {
+             return null;
+         }
+         try {
+            String result = "";
+            Statement s = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String query = "SELECT LAST_NAME, FIRST_NAME FROM APP.PROFS WHERE LOWER(FIRST_NAME) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(LAST_NAME) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(DEPARTMENT) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(SCHOOL) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(CURRENT_COMMITTEES) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(PAST_COMMITTEES) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(REQUESTED_COMMITTEES) LIKE TRIM(LOWER('" + input + "%'))";
+            ResultSet rs = s.executeQuery(query);
+            while (rs.next()) {
+                result += rs.getString("LAST_NAME") + ", " + rs.getString("FIRST_NAME") + "\n";
+            }
+            query = "SELECT NAME FROM APP.COMMS WHERE LOWER(NAME) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(DESCRIPTION) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(CURRENT_MEMBERS) LIKE TRIM(LOWER('" + input + "%'))";
+            query += " OR LOWER(CHAIR) LIKE TRIM(LOWER('" + input + "%'))";
+            rs = s.executeQuery(query);
+            while (rs.next()) {
+                result += rs.getString("NAME") + "\n";
+            }
+            return result;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
+}
